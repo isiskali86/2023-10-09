7b4718e8dec4b6896ff06e938730335c3d744b34
isnelys <isnelys.mendezramos@gmail.com>
2023-10-09 15:03:46 +0200

    Add button

--- tjs-app/src/App/App.jsx
+++ tjs-app/src/App/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/layouts/uis/Button/Button';
 
 function App() {
   return (
     <div className="App">
       Demat 
+      <hr/>
+      <Button text = "Valider"/>
+      <Button text = "Annuler"/>
+      <Button text = "Benjamin"/>
     </div>
   );
 }
