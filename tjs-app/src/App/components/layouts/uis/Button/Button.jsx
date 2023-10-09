e5debfefa1b7de7225103f9ef8bae0c6f9d80e7f
isnelys <isnelys.mendezramos@gmail.com>
2023-10-09 14:50:50 +0200

    Composant

---
+++ tjs-app/src/App/components/layouts/uis/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+/*function Button(props){
+    return(<button>Benjamin</button>);
+}*/
+
+const Button = (props) => {
+    return <button className= "Button">Benjamin</button>
+}
+
+export default Button;
